99bdbf5c157fb86ce6e15ca3b366bf45593aaeed
kazuki414 <iidam710@gmail.com>
2020-11-08 16:50:35 +0900

    0 ベースファイル
    git連携

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+// 本番環境用
+// ProductionならHTTPSに変更
+if (app()->environment('production')) {
+    URL::forceScheme('https');
+  }
