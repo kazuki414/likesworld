223095e9644dd225164dcbbd275c421efdfc7f5c
kazuki414 <iidam710@gmail.com>
2020-11-13 14:33:07 +0900

    9 top page読み込み処理追記

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,20 @@
-$(function(){
+$ (function(){
     $('.btn').click(function(){
         $('.cercle').css('opacity',0)
-        $('.cercle').css('witdh','100em')
+        $('.cercle').css('width','200em')
         $('.cercle').css('height','200em')
 
     });
 
+    // $(window).load(function(){
+    //     $('.cercle1').addClass('.dis')
+    // });
+    
+    $(document).ready(function(){
+        $('.cercle1').css('opacity',1)
+        $('.cercle2').css('opacity',1)
+        $('.cercle3').css('opacity',1)
+        $('.cercle4').css('opacity',1)
+        $('.cercle').css('opacity',1)
+    });
 });
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -6,6 +6,7 @@
     <title>LikesWorldにようこそ！</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 <div class="container mb-5">
@@ -38,13 +39,12 @@
 <div class="cercle4"></div>
 
 <div class="start mx-auto mt-5 text-center col-md-4">
-    <a href="#" type="button" class="btn btn-lg btn-block p-3 " role="button">Let's Start!!</a>
+    <a href="{{ route('top') }}" type="button" class="btn btn-lg btn-block p-3 " role="button">Let's Start!!</a>
     <div class="cercle"></div>
 </div>
 </div>
 <script src="{{ asset('js/script.js') }}"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
