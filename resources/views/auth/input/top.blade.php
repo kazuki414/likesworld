14efc400fb546ead702cda03e179d9ff06073f8f
kazuki414 <iidam710@gmail.com>
2020-11-16 17:25:24 +0900

    レイアウト調整
    branch design作成

--- resources/views/auth/input/top.blade.php
+++ resources/views/auth/input/top.blade.php
@@ -1,28 +1,37 @@
 @extends('layouts.base')
 @section('title','入力画面')
 @section('main')
+<div class="container text-center">
+<h2>好きなものを入力しよう</h2>
+<p>あなたの「好き」を登録して、世界を広げよう！</p>
+</div>
+
+<div class="container text-center mx-auto col-md-8">
 <form method="POST" action="{{ route('check') }}">
 @csrf
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">カテゴリタイプ</label>
-    <select name="type" class="form-control" id="exampleFormControlSelect1">
+  <div class="form-group col-md-6 mx-auto">
+    <label for="FormControlSelect1">カテゴリタイプ</label>
+    <select name="type" class="form-control" id="FormControlSelect1">
       <option value="0">好きな〇〇</option>
       <option value="1">A派？B派？</option>
     </select>
   </div>
-<div class="container">
+<div class="container form-group">
 <p>あなたの好きな</p>
-<input type="text" name="category" value="" placeholder="(例)食べ物" required>
+<label class="text-muted" for="category">カテゴリ：</label>
+<input type="text" id="category" name="category" value="" placeholder="(例)食べ物" required>
 <p>は</p>
-<input type="text" name="word" value="" placeholder="(例)ハンバーグ" required>
+<label class="text-muted" for="word">登録単語：</label>
+<input type="text" id="word" name="word" value="" placeholder="(例)ハンバーグ" required>
 <p>です。</p>
-<p>回答について</p>
-<input type="text" name="comment"  value="" placeholder="(例)〇〇なくらい　〇〇なため　etc" >
+<label class="text-muted" for="comment">回答についてのひとこと:</label>
+<textarea type="text" id="comment" name="comment" rows="3"  value="" placeholder="(例)〇〇なくらい　〇〇なため　etc" ></textarea>
 </div>
 <input type="submit" name="submit" value="登録する">
 </form>
+</div>
 
-<div class="container">
+<div class="mt-5 container">
 <p>例えば、こんなカテゴリも登録している方がいるようです</p>
 @foreach($categories as $category)
 <div class="btn">
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,22 +2,72 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
+<!-- ナビゲーターバー -->
+
 <div class="header">
-    <h2>ロゴ</h2>
+    <nav class="navbar flexed">
+        <div class="flex-shrink-0 flex items-center">
+        <a class="navbar-bland" href="{{ route('top')}}">
+        <img src="{{ asset('img/logo.png')}}"  width="50" height="50" alt="LikesWorldのロゴ">
+        <span class="navbar-brand mb-0 h1  text-dark">LikesWorld</span>
+        </a>
+        </div>
+
+    <!-- dropdownMenu -->
+        <div class="flex items-center nav-item dropdown">
+            <button class="nav-link dropdown-toggle" id="DropdownMenu"role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {{ Auth::user()->name }}
+            </button>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="DropdownMenu">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-jet-dropdown-link href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                        <span class="text-dark">
+                            ログアウト
+                        </span>
+                    </x-jet-dropdown-link>
+                </form>
+
+                <a class="dropdown-item" href="{{ route('mypage') }}">マイページ</a>
+                <a class="dropdown-item" href="{{ route('top') }}">トップページ</a>
+            </div>
+        </div>
+
+        
+      
+
+    </nav>
 </div>
 
+
+
+<!-- 内容 -->
+<div class = "m-5 p-5 mx-auto bg-white contents-container container">
     @section('main')
     <p>内容が指定されていません</p>
     @show
-
-<div class="footer">
-    <p class="text-center text-white bg-dark">Copyright(c) Kazuk.I. All Right Reserved.</p>
 </div>
+
+<footer class="bd-footer text-muted">
+<div class="container-fluid p-3 p-md-5">
+    <ul class="bd-footer-links">
+        <li class="col-md-3"><a href="{{ route('top') }}">TOPへ</a></li>
+        <li class="col-md-3"><a href="#">使い方</a></li>
+        <li class="col-md-3"><a href="#">作った人について</a></li>
+    </ul>
+    <p class="text-center">Copyright(c) Kazuk.I. All Right Reserved.</p>
+</div>
+</footer>
     
     
     <!-- Optional JavaScript -->
